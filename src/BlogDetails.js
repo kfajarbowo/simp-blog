f6ed74f924727d1510a486dd6b842a768f59ee59
kfajarbowo <bagas@workshop.web.id>
2021-02-10 01:22:29 +0700

    add content

---
+++ src/BlogDetails.js
@@ -0,0 +1,39 @@
+import { useParams,useHistory } from "react-router-dom";
+import useFetch from "./useFetch";
+
+
+const BlogDetails = () =>{
+    const { id } = useParams();
+    const {data: blog,error,isPending} = useFetch('http://localhost:8000/blogs/' + id);
+    const history = useHistory();
+
+    const handleClick = () =>{
+        fetch('http://localhost:8000/blogs/'+ blog.id,{
+            method: 'DELETE'
+        }).then(()=>{
+            history.push('/');
+        })
+    }
+    return(
+        <div className="blog-details">
+           { isPending && <div>Loading...</div> }
+           { error && <div>{error}</div> } 
+           { blog && (
+               <article>
+                   <h2>{blog.title}</h2>
+                   <div className="blog-body">{blog.body}</div>
+                   <p>Written by {blog.author}</p>
+                   <button onClick={handleClick}>Delete</button>
+               </article>
+           )}
+        </div>
+    );
+}
+
+export default BlogDetails;
+
+<style scoped>
+    p{
+
+    }
+</style>
